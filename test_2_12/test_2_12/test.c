d3adbbfe98ab54d9865fb02f1f58c381d7714a7b
一叶之秋 <1320611670@qq.com>
2021-02-12 21:24:10 +0800

    p25作业讲解

---
+++ test_2_12/test_2_12/test.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+
+void Init(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		 arr[i] = 0;
+	}
+}
+
+void print(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+void Reverse(int arr[], int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left < right)
+	{
+		int tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+		left++;
+		right--;
+	}
+}
+
+int main()
+{
+	//int arr[10] = { 1,2,3,4,5,6,7,8,9 ,10};
+	//int sz = sizeof(arr) / sizeof(arr[0]);
+	////Init(arr, sz);
+	//print(arr, sz);
+	//Reverse(arr, sz);
+	//print(arr, sz);
+	//return 0;
+
+	int arr1[] = { 1,3,5,7,9 };
+	int arr2[] = { 2,4,6,8,10 };
+	int tmp = 0;
+	int i = 0;
+	int sz = sizeof(arr1) / sizeof(arr1[0]);
+	for (i = 0; i < sz; i++)
+	{
+		tmp = arr1[i];
+		arr1[i] = arr2[i];
+		arr2[i] = tmp;
+	}
+	return 0;
+
+}
